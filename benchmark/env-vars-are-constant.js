7a25f326685e1e1dd58ec33ae1d9a6790cb94878
Karim Alibhai <karim@alibhai.co>
2019-02-01 19:06:01 -0500

    Add: object form of process.env

--- benchmark/env-vars-are-constant.js
+++ benchmark/env-vars-are-constant.js
@@ -7,6 +7,15 @@ suite('env variables are constant', () => {
     return a + b
   }
 
+  const env = JSON.parse(JSON.stringify(process.env))
+  function addSemiFast(a, b) {
+    if (env.NODE_ENV === 'test') {
+      // do something special
+    }
+
+    return a + b
+  }
+
   // The right way to do it:
   const isTestEnv = process.env.NODE_ENV === 'test'
   function addFast(a, b) {
@@ -18,5 +27,6 @@ suite('env variables are constant', () => {
   }
 
   bench('with env variable access', () => addSlow(1, 1))
+  bench('with env as real object', () => addSemiFast(1, 1))
   bench('without env variable access', () => addFast(1, 1))
 })
